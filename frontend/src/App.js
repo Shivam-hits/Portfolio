c28f4b830ee744d01973b99852a45c3f123fb6f7
Shivam Awasthi <shivamhits1972@gmail.com>
2023-08-19 22:27:10 +0530

    creation of pages and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
 
 function App() {
   return (
     <div className="App">
-     
     </div>
   );
 }
-
 export default App;
