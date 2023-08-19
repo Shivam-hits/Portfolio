699472b85780ee70fb8f34a66bf027a652f998aa
Shivam Awasthi <shivamhits1972@gmail.com>
2023-08-19 23:38:15 +0530

    HomePage

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Header from "../components/header/header.jsx"
+import Box from "../components/box/box.jsx"
+import Latest from "../components/latest/latest.jsx"
+import Footer from "../components/footer/footer.jsx"
+import Skills from "../components/skills/skills.jsx"
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      <Header />
+      <Box />
+      <Latest />
+      <Skills />
+      <Footer />
+    </div>
   )
 }
 
