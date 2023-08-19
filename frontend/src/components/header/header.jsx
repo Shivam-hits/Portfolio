88dc7e4ebd7b350378340e6366a6df795b09f0cc
Shivam Awasthi <shivamhits1972@gmail.com>
2023-08-20 00:39:02 +0530

    Header navbar

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -3,12 +3,14 @@ import "./header.css"
 function Header() {
   return (
     <div className='header_container'>
+
       <div className='header_navbar'>
         <h3>Act as Shivam</h3>
-        <h3>About Me</h3>
-        <h3>Contact Me</h3>
+        <div className='header_navbar_2'>
+          <h3>About Me</h3>
+          <h3>Contact Me</h3>
+        </div>
       </div>
-      header
     </div>
   )
 }
--- frontend/src/components/latest/latest.jsx
+++ frontend/src/components/latest/latest.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Latest() {
   return (
-    <div>Latest</div>
+    <div>
+      Latest
+    </div>
   )
 }
 
